82d4f58ec7136c700da013c38c947b095c8c384c
Bogdan Parubok <bogdan.parubok@hotmail.com>
2023-02-06 00:36:30 +0200

    Adds lowest and highest consumption text.

--- csv_graph_draw.js
+++ csv_graph_draw.js
@@ -23,7 +23,7 @@ function drawCSVgraph(){
             let svgWidth = svgContainer.getBoundingClientRect().width;
             let offsetFromEnd = 50; // in pixels
             let endPosition = svgWidth-offsetFromEnd; // Defines how far the horisontal graph should go
-            let endPositionStrokes = endPosition - 100; // Defines how far the strokes on horisontal graph should go
+            let strokesEndPosition = endPosition - 100; // Defines how far the strokes on horisontal graph should go
             // Get SVG container first group, the graph vertical and horisontal lines
             let g =  document.getElementById("graph");
             // X and Y coordinates for lines
@@ -37,11 +37,11 @@ function drawCSVgraph(){
                 g.innerHTML += `<line x1="${coordinates.x[i]}" y1="${coordinates.y[i]}" x2="${coordinates.x[i+1]}" y2="${coordinates.y[i+1]}" />`;
             }
             // Small vertical strokes on horisontal line
-            const hzWidth = endPositionStrokes/25;
+            const hzWidth = strokesEndPosition/25;
             for(let i = 1; i < 25; i++) {
                 g.innerHTML += `<line x1="${60 + hzWidth*i}" y1="${200}" x2="${60 + hzWidth*i}" y2="${205}" />`;
             }
-            // Draw small vertical strokes
+            // Find highest and lowest consumption
             let maxConsumption = 0;
             let firstResultsToIgnore = 12;
             for (let i = firstResultsToIgnore; i < results.data.length-1; i++) {
@@ -50,13 +50,23 @@ function drawCSVgraph(){
                     maxConsumption = results.data[i][4];
                 }
             }
+            let minConsumption = results.data[12][4];
+            for (let i = firstResultsToIgnore; i < results.data.length-1; i++) {
+                if(results.data[i][4] <= minConsumption) {
+                    minConsumption = results.data[i][4];
+                }
+            }
+            document.getElementById("highestConsumption").innerHTML = `Period highest consumption: ${maxConsumption} KWh`;
+            document.getElementById("lowestConsumption").innerHTML = `Period lowest consumption: ${minConsumption} KWh`;
+            // Add small strokes to vertical graph
             let widthBetweenPoints = 150/8;
             let y = 200 - widthBetweenPoints;
             for (let i = 0; i < 8; i++) {
                 g.innerHTML += `<line x1="${60}" y1="${y}" x2="${55}" y2="${y}"/>`;
                 y -= widthBetweenPoints;
             }
-            let middleOfHorisontalGraph = endPositionStrokes/2;
+            // Adds text to the graph
+            let middleOfHorisontalGraph = strokesEndPosition/2;
             let textGroup = document.getElementById("text");
             textGroup.style.fontSize = "8px";
             textGroup.style.fontFamily = "arial";
