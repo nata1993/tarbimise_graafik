a399330e628d1037cdf8ef4d9b74d103197b2d41
Bogdan Parubok <bogdan.parubok@hotmail.com>
2023-03-09 00:58:58 +0200

    Finalizes cost graph.

--- Cost_Graph.js
+++ Cost_Graph.js
@@ -16,7 +16,6 @@ function drawCostGraph(Elering_Data, CSV_Data, horizontalWidthBetweenStrokes){
     // Variables
     const Graph_Offset_From_End = 50; // Defines offset from SVG container end in pixels
     const Horizontal_Graph_End_Position = Cost_SVG_Width - Graph_Offset_From_End; // Defines how far the horisontal graph should go
-    const Horizontal_Graph_End_Position_Of_Strokes = Horizontal_Graph_End_Position - 86; // Defines how far the strokes on horisontal graph should go
 
     // X and Y coordinates for vertical and horizontal graph lines
     const Base_Graph_Coordinates = {
@@ -28,29 +27,38 @@ function drawCostGraph(Elering_Data, CSV_Data, horizontalWidthBetweenStrokes){
     const length = Base_Graph_Coordinates.X.length;
     let baseStr = "";
     for (let i = 0; i < length; i+=2) {
-        baseStr += `<line x1="${Base_Graph_Coordinates.X[i]}" y1="${Base_Graph_Coordinates.Y[i]}" x2="${Base_Graph_Coordinates.X[i+1]}" y2="${Base_Graph_Coordinates.Y[i+1]}" />`;
+        baseStr += `<line x1="${Base_Graph_Coordinates.X[i]}" y1="${Base_Graph_Coordinates.Y[i]}"
+                    x2="${Base_Graph_Coordinates.X[i+1]}"y2="${Base_Graph_Coordinates.Y[i+1]}" />`;
     }
-
     Base_Graph.innerHTML = baseStr;
 
     // Calculate cost for each hour of spending
     let cost_data = [];
-    
-    for (let i = 0; i < Elering_Data.length; i++) {
-        let j = 11;
-        cost_data.push(Number(Number(Elering_Data[i].price * Number(CSV_Data[j][4].replace(",", "."))).toFixed(3)));
-        j++;
+    let eleringData_length = Elering_Data.length - 2;
+    for (let i = 0; i < eleringData_length; i++) {
+        cost_data.push((Elering_Data[i].price * CSV_Data[i]) / 10); // Divide by 10 because elering data is in MWh, not KWh
     }
-    console.log(cost_data);
 
-    // Find highest cost
+    // Find highest and average cost as well as when and what etc
     let highestCost = cost_data[0];
-    for(let i = 0; i < cost_data.length; i++) {
-        if(cost_data[i] > highestCost) {
+    let averageCost = 0;
+    let whenHighestCost = null;
+    let whatElectricityPrice = 0;
+    let whatConsumption = 0;
+    const costDataLength = cost_data.length;
+    for(let i = 0; i < costDataLength; i++) {
+        // Highest cost
+        if(cost_data[i] >= highestCost) {
             highestCost = cost_data[i];
+            whenHighestCost = Elering_Data[i].timestamp;
+            whatElectricityPrice = Elering_Data[i].price;
+            whatConsumption = CSV_Data[i];
         }
+        // Sum for average cost
+        averageCost += cost_data[i];
     }
-    console.log(highestCost);
+    averageCost = averageCost/costDataLength;
+    const highest_consumption_date = new Date(whenHighestCost); // invalid date...
 
     // Map cost data to cost graph
     let lineStr = "";
@@ -65,4 +73,7 @@ function drawCostGraph(Elering_Data, CSV_Data, horizontalWidthBetweenStrokes){
     }
     Cost_Graph.innerHTML += lineStr;
 
+    document.getElementById("highestCost").innerHTML = `Period highest cost of consumption: ${highestCost.toFixed(3)} \u00A2`;
+    document.getElementById("whenCost").innerHTML = `Which was consumed on ${highest_consumption_date} with the electricity price of ${whatElectricityPrice} \u00A2/KWh. The consumed electricity was ${whatConsumption} KWh.`;
+    document.getElementById("averageCost").innerHTML = `Period average cost of consumption: ${averageCost.toFixed(3)} \u00A2`;
 }
