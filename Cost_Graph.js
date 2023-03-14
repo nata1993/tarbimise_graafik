054ffa78a2adf19aa5cb6aff0dce4793878ce15f
Bogdan Parubok <bogdan.parubok@hotmail.com>
2023-03-14 15:24:10 +0200

    Adds type for cost statistics.

--- Cost_Graph.js
+++ Cost_Graph.js
@@ -94,7 +94,7 @@ function drawCostGraph(Elering_Data, CSV_Data, horizontalWidthBetweenStrokes){
     Graph_Title.innerHTML += `<text x="${(Cost_SVG_Width / 2) - 100}" y="640">Consumption cost graph</text>`;
 
     document.getElementById("averageCost").innerHTML = `${averageCost.toFixed(3)} \u00A2`;
-    document.getElementById("weightedCost").innerHTML = `${weightedCost.toFixed(3)} \u00A2`;
+    document.getElementById("weightedCost").innerHTML = `${weightedCost.toFixed(3)} \u00A2/KWh`;
     document.getElementById("highestCost").innerHTML = `${highestCost.toFixed(3)} \u00A2`;
     document.getElementById("whenCost").innerHTML = `Which was consumed on ${highest_consumption_date} with the electricity price of ${whatElectricityPrice.toFixed(3)} \u00A2/KWh. The consumed electricity was ${whatConsumption} KWh.`;
 }
