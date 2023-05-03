53f06a608057814a31ae07d94b0836acd22a1fc7
Bogdan Parubok <bogdan.parubok@hotmail.com>
2023-05-03 19:45:37 +0300

    Removes most of the old prototype code from the project.

--- Cost_Graph.js
+++ Cost_Graph.js
@@ -1,52 +1,5 @@
 // Draw graph from CSV file
 function drawCostGraph(Merged_Data, horizontalWidthBetweenStrokes, Length_Without_Null){
-    // Get SVG container width
-    const Cost_SVG_Width = document.getElementById("NPS_CSV_Cost").getBoundingClientRect().width;
-
-    // Find elements to work with
-    const Cost_Graph = document.getElementById("costGraph");
-    const Text_Group = document.getElementById("costText");
-    const Base_Graph = document.getElementById("costBaseGraph");
-    const Graph_Title = document.getElementById("graphTwoTitle");
-
-    // Clear the contents of SVG before redrawing
-    Cost_Graph.innerHTML = "";
-    Text_Group.innerHTML = "";
-    Base_Graph.innerHTML = "";
-    Graph_Title.innerHTML = "";
-
-    // Variables
-    const Graph_Offset_From_End = 70; // Defines offset from SVG container end in pixels
-    const Horizontal_Graph_End_Position = Cost_SVG_Width - Graph_Offset_From_End; // Defines how far the horisontal graph should go
-    const base_y = 600;
-    const base_x = 60;
-
-    // X and Y coordinates for vertical and horizontal graph lines
-    const Base_Graph_Coordinates = {
-        X : [base_x, base_x, base_x, Horizontal_Graph_End_Position, base_x, 58, base_x, 62, Horizontal_Graph_End_Position, Horizontal_Graph_End_Position, Horizontal_Graph_End_Position, Horizontal_Graph_End_Position],
-        Y : [350, base_y, base_y, base_y, 350, 355, 350, 355, base_y, base_y, 350, base_y+7]
-    };
-
-    // For the sake of it, X and Y are pairs so X and Y arrays are the same length
-    const length = Base_Graph_Coordinates.X.length;
-    let baseStr = "";
-    for (let i = 0; i < length; i+=2) {
-        baseStr += `<line x1="${Base_Graph_Coordinates.X[i]}" y1="${Base_Graph_Coordinates.Y[i]}"
-                          x2="${Base_Graph_Coordinates.X[i+1]}"y2="${Base_Graph_Coordinates.Y[i+1]}" />`;
-    }
-    // Strokes on horizontal verticle
-    for(let i = 0; i < Merged_Data.length; i += 24) {
-        baseStr += `<line x1="${base_x + (horizontalWidthBetweenStrokes * i)}" y1="${base_y}"
-                          x2="${base_x + (horizontalWidthBetweenStrokes * i)}" y2="${base_y+7}" />`;
-    }
-    Base_Graph.innerHTML = baseStr;
-
-    // Calculate cost for each hour of spending
-    let cost_data = [];
-    for (let i = 0; i < Length_Without_Null; i++) {
-        cost_data.push(Merged_Data[i]["price"] * Merged_Data[i]["consumption"]);
-    }
-
     // Find highest and average cost as well as when and what etc
     let highestCost = cost_data[0];
     let averageCost = 0;
@@ -69,22 +22,5 @@ function drawCostGraph(Merged_Data, horizontalWidthBetweenStrokes, Length_Withou
     averageCost = (averageCost / costDataLength).toFixed(3);
     const highest_consumption_date = new Date(whenHighestCost).toLocaleDateString("fi-FI");
 
-    // Map cost data to cost graph
-    let lineStr = "";
-    const cost_ratio = 200 / highestCost;
-    let x1 = 61;
-    let x2 = 61 + horizontalWidthBetweenStrokes;
-    for(let i = 0; i < Length_Without_Null; i++) {
-        let y = base_y - cost_data[i] * cost_ratio;
-        lineStr += `<line id="cost${cost_data[i]}" x1="${x1}" y1="${y}" x2="${x2}" y2="${y}" stroke="#000" stroke-width="2" />`;
-        x1 += horizontalWidthBetweenStrokes;
-        x2 += horizontalWidthBetweenStrokes;
-    }
-    Cost_Graph.innerHTML += lineStr;
-
-    Graph_Title.style.fontFamily = "arial";
-    Graph_Title.style.fontSize = "16px";
-    Graph_Title.innerHTML += `<text x="${(Cost_SVG_Width / 2) - 100}" y="640">Consumption cost graph</text>`;
-    
     document.getElementById("whenCost").innerHTML = `Which was consumed on ${highest_consumption_date} with the electricity price of ${whatElectricityPrice.toFixed(3)} \u00A2/KWh. The consumed electricity was ${whatConsumption} KWh.`;
 }
--- NPS_CSV_Graph.js
+++ NPS_CSV_Graph.js
@@ -214,120 +214,8 @@ function NPS_CSV_Graph_Generator(CSV_File_Results) {
                 "test1"
             );
 
-
-
-            // Get SVG parent container calculated width
-            const SVG_Width = document.getElementById("NPS_CSV_Cost").getBoundingClientRect().width;
-
-            // Get SVG container base elements
-            const Base_Graph = document.getElementById("npsBaseGraph");
-            const Verticle_Group = document.getElementById("npsPriceVector");
-            const Text_Group = document.getElementById("npsText");
-            const Data_Points_Group = document.getElementById("csvConsumptionVector");
-            const Graph_Title = document.getElementById("graphOneTitle");
-
-            // Clear SVG contents if there has been drawn previously something 
-            Base_Graph.innerHTML = "";
-            Verticle_Group.innerHTML = "";
-            Data_Points_Group.innerHTML = "";
-            Text_Group.innerHTML = "";
-            Graph_Title.innerHTML = "";
-
-            // Graph sizing
-            const offsetFromEnd = 70;   // Defines offset from SVG container end in pixels
-            const endPosition = SVG_Width - offsetFromEnd; // Defines base graph horizontal graph end position
-            const base_y = 275;
-            const base_x = 60;
-            const graphheight = 200;
-            
-            // Coordinates for base graph vectors starts and ends
-            const baseGraphCoordinates = {
-                x: [base_x, 58, base_x, 62, base_x, base_x, base_x, endPosition+5, endPosition, endPosition, endPosition, endPosition-2, endPosition, endPosition+2],
-                y: [50, 55, 50, 55, 50, base_y, base_y, base_y, base_y+7, 50, 50, 55, 50, 55, 50]
-            };
-
-            // Draw base graph
-            for(let i = 0; i < baseGraphCoordinates.x.length; i+=2) {
-                Base_Graph.innerHTML += `<line x1="${baseGraphCoordinates.x[i]}" y1="${baseGraphCoordinates.y[i]}"
-                                        x2="${baseGraphCoordinates.x[i+1]}" y2="${baseGraphCoordinates.y[i+1]}" />`;
-            }
-
-            // Draws small strokes to base graph horisontal line - needs improvements
-            const eDataStart = 0;
-            const eDataEnd = EleringData._EleringDataLength;
-            const countOfDataPoints = eDataEnd - eDataStart;
-            const horizontalWidthBetweenStrokes = (endPosition - 61)/ countOfDataPoints;
-            let strokesStr = "";
-            for(let i = 0; i < EleringData._EleringDataLength; i += 24) {
-                strokesStr += `<line x1="${base_x + (horizontalWidthBetweenStrokes * i)}" y1="${base_y}"
-                                     x2="${base_x + (horizontalWidthBetweenStrokes * i)}" y2="${base_y+7}" />`;
-            }
-            
-            // Draws small strokes to base graph vertical line on the left side - also needs improvements to reduce those damn iffffffsssssss
-            let nRatio = priceRatio(Math.ceil(Statistics._HighestPriceOfElectricity)); // Ratio number to display next to vertical graph. Essentially a graph segmentation ratio.
-            let width = 0;
-            const highestPriceLevel = Statistics._HighestPriceOfElectricity / nRatio;
-            const verticalWidthBetweenPoints = graphheight / highestPriceLevel;
-            for (let i = 0; i < highestPriceLevel + 1; i++) {
-                width = base_y - (i * verticalWidthBetweenPoints);
-                strokesStr += `<line x1="${base_x}" y1="${width}" x2="${53}" y2="${width}" />`;
-            }
-
-            // Draws small strokes to base graph vertical line on the right side
-            let widthBetweenPoints = graphheight / 8;
-            let y = base_y - widthBetweenPoints;
-            for (let i = 0; i < 8; i++) {
-                strokesStr += `<line x1="${endPosition}" y1="${y}" x2="${endPosition+7}" y2="${y}"/>`;
-                y -= widthBetweenPoints;
-            }
-            Base_Graph.innerHTML += strokesStr;
-
-            // Draws continuous line of prices on graph
-            const pricelevel1 = 6;
-            const pricelevel2 = 12;
-            const extremepricelevel = 100;
-            let x1 = 61;
-            let x2 = 61 + horizontalWidthBetweenStrokes;
-            const price_ratio = graphheight / Statistics._HighestPriceOfElectricity; // Ratio between 150px of vertical graph length and highest price
-            let graphStr = "";
-            for (let i = eDataStart; i < eDataEnd; i++) {
-                const hourPrice = Merged_Data._MergedData[i]["price"];
-                let y = base_y - hourPrice * price_ratio;
-                if (hourPrice <= pricelevel1) {
-                    graphStr += `<line x1="${x1}" y1="${y}" x2="${x2}" y2="${y}" stroke="#0A0" stroke-width="3"/>`;
-                }
-                else if (hourPrice > pricelevel1 && hourPrice <= pricelevel2) {
-                    graphStr += `<line x1="${x1}" y1="${y}" x2="${x2}" y2="${y}" stroke="#FE0" stroke-width="3"/>`;
-                }
-                else if (hourPrice >= extremepricelevel) {
-                    graphStr += `<circle cx="${x1}" cy="${y}" r="1.5" stroke="#F0F" fill="#F0F"/>`;
-                    graphStr += `<circle cx="${x1+(horizontalWidthBetweenStrokes/2)}" cy="${y}" r="1.5" stroke="#F0F" fill="#F0F"/>`;
-                    graphStr += `<circle cx="${x2}" cy="${y}" r="1.5" stroke="#F0F" fill="#F0F"/>`;
-                    graphStr += `<line x1="${x1}" y1="${y}" x2="${x2}" y2="${y}" stroke="#F0F"/>`;
-                }
-                else {
-                    graphStr += `<line x1="${x1}" y1="${y}" x2="${x2}" y2="${y}" stroke="#F00" stroke-width="3"/>`;
-                }
-                x1 += horizontalWidthBetweenStrokes;
-                x2 += horizontalWidthBetweenStrokes;
-            }
-            Verticle_Group.innerHTML = graphStr;
-
-            // Adds CSV file datapoints to the graph in second group of SVG container;
-            let lineStr = "";
-            const consumption_ratio = graphheight / Statistics._HighestConsumption;
-            x1 = 61;
-            x2 = 61 + horizontalWidthBetweenStrokes;
-            for(let i = 0; i < CSV_Normalized_Data_Length; i++) {
-                let consumption = Merged_Data._MergedData[i]["consumption"];
-                let y = base_y - consumption * consumption_ratio;
-                lineStr += `<line id="${consumption}" x1="${x1}" y1="${y}" x2="${x2}" y2="${y}" stroke="#000" stroke-width="2" />`;
-                x1 += horizontalWidthBetweenStrokes;
-                x2 += horizontalWidthBetweenStrokes;
-            }
-            Data_Points_Group.innerHTML += lineStr;
-
             // Add text to graph
+            /*
             let textOffsetBelowGraph = base_y + 25;
             let textStr = "";
             Text_Group.style.fontFamily = "arial";
@@ -371,7 +259,9 @@ function NPS_CSV_Graph_Generator(CSV_File_Results) {
             Graph_Title.style.fontSize = "16px";
             Graph_Title.innerHTML += `<text x="${(SVG_Width/2) - 135}" y="${textOffsetBelowGraph + 25}">NPS price and electricity consumption</text>`;;
 
-            // Fill lowest and highest prices as well as consumption to HTML
+            */
+
+            // Fill data statistics to HTML
             document.getElementById("period").innerHTML = `Period: ${ConsumptionData._ConsumptionDataPeriod}`;
 
             document.getElementById("highestPrice").innerHTML = `${Statistics._HighestPriceOfElectricity} \u00A2/KWh`;
@@ -389,9 +279,6 @@ function NPS_CSV_Graph_Generator(CSV_File_Results) {
             document.getElementById("dayTimeConsumption").innerHTML = `${ConsumptionData._DayTimeConsumption} KWh`;
             document.getElementById("nightTimeConsumption").innerHTML = `${ConsumptionData._NightTimeConsumption} KWh`;
             document.getElementById("totalConsumption").innerHTML = `${ConsumptionData._ConsumptionDataTotalConsumption} KWh`;
-        
-            // Draw second graph - from Cost_Graph.js file
-            drawCostGraph(Merged_Data._MergedData, horizontalWidthBetweenStrokes, Merged_Data._MergedDataWithoutNull);
         })
         .catch(err => { throw err });
 }
