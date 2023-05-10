13cef9f1dafd82144bfd89cc6781cbccbc89c8af
Bogdan Parubok <bogdan.parubok@hotmail.com>
2023-05-10 12:05:46 +0300

    Removes redundant comment.

--- NPS_CSV_Graph.js
+++ NPS_CSV_Graph.js
@@ -1,23 +1,4 @@
 
-/*
-    Graph drawing is made in order:
-    1) Clearing graph container
-    2) Drawing base graph
-        - Vertical and horizontal graph vectors with arros on the end.
-    3) Drawing strokes on horizontal line
-        - Small vertical strokes representing hours
-    4) Drawing strokes on vertical line
-        - Small horizontal strokes representing price segment with VAT
-    5) Drawing real price vector
-        - Continuos line representing price level on the graph. Graph
-        height depends on Nord Pool Spot prices. VAT included.
-    6) Draw text on the graph
-        - Text of hours on below horizontal graph
-        - Text of price segments next to vertical graph with VAT
-        - basic graph meaning text next to both horizontal and vertical graphs
-*/
-
-
 // Draw Nord Pool Spot prices based on user selected date range
 function drawGraphs() {
     // Retrieve file from input
